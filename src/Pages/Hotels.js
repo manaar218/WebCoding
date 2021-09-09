2b4cf8fc081c2bb00565c46838e94c18e88336fa
manaar218 <76546179+manaar218@users.noreply.github.com>
2021-09-09 13:52:47 +0530

    Added Hotels.js page

---
+++ src/Pages/Hotels.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import DatePickers from '../Components/DateTimePicker';
+
+export default function Hotels() {
+  return (
+    <React.Fragment>
+        <Container fixed>
+            
+        </Container>
+    </React.Fragment>
+  );
+}
