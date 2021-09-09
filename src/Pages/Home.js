d842e2491c135dc2e44748ee13c9b5bda0c9aa3b
manaar218 <76546179+manaar218@users.noreply.github.com>
2021-09-09 13:52:37 +0530

    Added Home.js page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import DatePickers from '../Components/DateTimePicker';
+import SimpleCard from '../Components/HomeMultiCard';
+import ContainedButtons from '../Components/Buttons1';
+import BasicButtonGroup from '../Components/BasicButtonGroup';
+import FloatingActionButtons from '../Components/FloatingActionButton';
+import MaterialUIPickers from '../Components/DateTimePicker';
+import LabelBottomNavigation from '../Components/BottomNavBar';
+import SimpleBreadcrumbs from '../Components/Breadcrumbs';
+import CustomizedRadios from '../Components/RadioGroup';
+import ControlledAccordions from '../Components/ControlledAccordion';
+
 
 export default function Home() {
   return (
     <React.Fragment>
         <Container fixed>
-            <p>
-                Welcome to your web application. Make something beautiful!
-            </p>
-            <p>
-                To get started, edit <code>src/App.js</code>, save, and reload this page.
-            </p>
-            <DatePickers/>
+
+           {/* <SimpleBreadcrumbs/>
+           <ContainedButtons/>
+           <BasicButtonGroup/>
+           <CustomizedRadios/>
+           <ControlledAccordions/>
+          
+            <SimpleCard/>
+
+            <MaterialUIPickers/>
+            <FloatingActionButtons/>
+            <LabelBottomNavigation/>  */}
+            
         </Container>
     </React.Fragment>
   );
