cbc5eea65b00149c8b40b9b70e1337343df04a0e
manaar218 <76546179+manaar218@users.noreply.github.com>
2021-09-09 13:50:44 +0530

    Updated package.json, src

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import ButtonAppBar from './Components/AppBar.js'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Pages/Home.js'
 import Gallery from './Pages/Gallery.js'
+import Hotels from './Pages/Hotels.js'
+import PrimarySearchAppBar from './Components/AppBarSearch';
+import SeasonsPage from './Pages/SeasonsPage';
+import Episode100 from './Pages/Episode100';
 
 
 function App() {
@@ -10,7 +14,8 @@ function App() {
     <div className="App">
       {/* Header (Navigation Bar) */}
       <header className="App-header">
-      <ButtonAppBar/>
+      
+      <PrimarySearchAppBar/>
       </header>
       
       {/* Router - Pages */}
@@ -18,6 +23,9 @@ function App() {
             <Route path={process.env.PUBLIC_URL + '/'} exact > <Home/> </Route>
             <Route path={process.env.PUBLIC_URL + '/home'} > <Home/> </Route>
             <Route path={process.env.PUBLIC_URL + '/gallery'}  > <Gallery/> </Route>
+            <Route path={process.env.PUBLIC_URL + '/hotels'}  > <Hotels/> </Route>
+            <Route path={process.env.PUBLIC_URL + '/kuch-rang-pyaar-ke-aise-bhi-seasons'}  > <SeasonsPage/> </Route>
+            <Route path={process.env.PUBLIC_URL + '/kuch-rang-pyaar-ke-aise-bhi-season-1-episode-100'}  > <Episode100/> </Route>
           </Switch>
 
       <footer className="App-footer">
