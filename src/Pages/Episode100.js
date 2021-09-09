b7da7d03b4a0751ec5802cd1dc41f8e8c5fa877f
manaar218 <76546179+manaar218@users.noreply.github.com>
2021-09-09 14:48:30 +0530

    Added component to SeasonButtons.js

--- src/Pages/Episode100.js
+++ src/Pages/Episode100.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import ButtonAppBar from '../Components/AppBar';
+import OutlinedButtons from '../Components/SeasonButtons';
 
 export default function Episode100() {
   return (
     <React.Fragment>
         <Container fixed>
 
-           
+         <OutlinedButtons/>  
             
         </Container>
     </React.Fragment>
